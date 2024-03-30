5eb1f50e2c8bd1d952c10dceaa987df535f8c788
Kawe Peixinho <141229485+KawePeixinho@users.noreply.github.com>
2024-03-29 21:57:53 -0300

    update

---
+++ python_estudos/aula58.py
@@ -0,0 +1,11 @@
+#strings imutaveis
+
+string = 'kawe' #criando uma variavel chamada string
+#string[2] = 'b' #tentando atribuir o valor 'b' ao indice [2] da variavel
+#print(string[3]) #exibindo na tela
+
+# dará erro, pois esse tipo de variavel, no caso a string, int, bool e float, sao do tipo imutaveis
+#ela não sofrem alterações
+
+print(string.capitalize()) #metodo string que retorna a primeira letra maiuscula
+
